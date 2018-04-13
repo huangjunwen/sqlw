6d705f881e21b8c8ccc2636117347262c701bddb
jayven <kassarar@gmail.com>
2018-04-13 16:05:17 +0800

    Seperate directives

---
+++ stmtx/directive/replace.go
@@ -0,0 +1,37 @@
+package directive
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/beevik/etree"
+	"github.com/huangjunwen/sqlwrapper/dbctx"
+	"github.com/huangjunwen/sqlwrapper/stmtx"
+)
+
+type replaceDirective struct {
+	origin string
+	with   string
+}
+
+func (d *replaceDirective) Initialize(ctx *dbctx.DBContext, stmt *stmt.StmtInfo, tok etree.Token) error {
+	elem := tok.(*etree.Element)
+	with := elem.SelectAttrValue("with", "")
+	if with == "" {
+		return fmt.Errorf("Missing 'with' attribute in <replace> directive")
+	}
+	d.origin = elem.Text()
+	d.with = with
+	return nil
+}
+
+func (d *replaceDirective) Generate() (string, error) {
+	return d.with, nil
+}
+
+func (d *replaceDirective) GenerateQuery() (string, error) {
+	return d.origin, nil
+}
+
+func (d *replaceDirective) ProcessQueryResult(resultColumnNames *[]string, resultColumnTypes *[]*sql.ColumnType) error {
+	return nil
+}
